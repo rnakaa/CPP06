e17fcb95146a0db012effa32ea63609a3db04008
rnakaa <102458195+rnakaa@users.noreply.github.com>
2024-06-29 15:02:30 +0900

    ex00 done

--- ex00/ScalarConverte.cpp
+++ ex00/ScalarConverte.cpp
@@ -2,23 +2,108 @@
 
 void ScalarConverte::convert(const std::string &prm){
 	LiteralType literaltype = checkLiteral(prm);
-	std::cout << literaltype << std::endl;
+
+	if (literaltype == CHR){
+		convertFromChar(prm[0]);
+	}
+	else if (literaltype == INT){
+		convertFromInt(std::stoi(prm));
+	}
+	else if (literaltype == FLT){
+		convertFromFloat(std::stof(prm));
+	}
+	else if (literaltype == DBL){
+		convertFromDouble(std::stod(prm));
+	}
+	else if (literaltype == ERR){
+		std::cerr << "ERR : input is invalid" << std::endl;
+	}
+
+}
+
+void ScalarConverte::convertFromChar(char c) {
+	std::cout << "char: '" << c << "'" << std::endl;
+	std::cout << "int: '" << static_cast<int>(c) << "'" << std::endl;
+	std::cout << "float: '" << static_cast<float>(c) << ".0f" << std::endl;
+	std::cout << "double: '" << static_cast<double>(c) << ".0" << std::endl;
+}
+
+void ScalarConverte::convertFromInt(int i) {
+	std::cout << "char: ";
+	if (i >= 32 && i <= 126)
+		std::cout << "'" << static_cast<char>(i) << "'" << std::endl;
+	else
+		std::cout << "Non displayable" << std::endl;
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
+}
 
 
+void ScalarConverte::convertFromFloat(float f) {
+	std::cout << "char: ";
+	if (std::isnan(f) || std::isinf(f))
+			std::cout << "impossible" << std::endl;
+		else if (f >= 32 && f <= 126)
+			std::cout << "'" << static_cast<char>(f) << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
+
+		std::cout << "int: ";
+		if (std::isnan(f) || std::isinf(f) || f > 2147483647 || f < -2147483648)
+			std::cout << "impossible" << std::endl;
+		else
+			std::cout << static_cast<int>(f) << std::endl;
+
+		if (std::floor(f) == f){
+		std::cout << "float: " << f << ".0f" << std::endl;
+		std::cout << "double: " << static_cast<double>(f) << ".0" << std::endl;
+		}
+		else{
+		std::cout << "float: " << f  << std::endl;
+		std::cout << "double: " << static_cast<double>(f) << std::endl;
+			
+		}
+}
+
+void ScalarConverte::convertFromDouble(double d) {
+	std::cout << "char: ";
+	if (std::isnan(d) || std::isinf(d))
+			std::cout << "impossible" << std::endl;
+		else if (d >= 32 && d <= 126)
+			std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
+
+		std::cout << "int: ";
+		if (std::isnan(d) || std::isinf(d) || d > 2147483647 || d < -2147483648)
+			std::cout << "impossible" << std::endl;
+		else
+			std::cout << static_cast<int>(d) << std::endl;
+
+		if (std::floor(d) == d){
+		std::cout << "float: " << static_cast<float>(d) << ".0f" << std::endl;
+		std::cout << "double: " << d << ".0" << std::endl;
+		}
+		else {
+		std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
+		std::cout << "double: " << d << std::endl;
+			
+		}
 }
 
 LiteralType ScalarConverte::checkLiteral(const std::string &prm){
 	if (isChar(prm)){
-		std::cout << "is Char" << std::endl;
 		return CHR;
 	}
 	else if (isInt(prm)){
-		std::cout << "is Int" << std::endl;
 		return INT;
 	}
 	else if (isFloat(prm)){
-		std::cout << "is Flout" << std::endl;
 		return FLT;
+	}
+	else if (isDouble(prm)){
+		return DBL;
 	}
 	return ERR;
 }
@@ -59,29 +144,14 @@ bool ScalarConverte::isFloat(std::string prm){
 }
 
 bool ScalarConverte::isDouble(std::string prm){
-	if (prm.size() == 1 && !std::isdigit(prm[0])) {
+	try {
+		std::stod(prm);
 		return true;
 	}
-	return false;
+	catch(...){
+		return false;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- ex00/ScalarConverte.hpp
+++ ex00/ScalarConverte.hpp
@@ -19,6 +19,10 @@ class ScalarConverte{
 		static bool isInt(const std::string &prm);
 		static bool isFloat(std::string prm);
 		static bool isDouble(std::string prm);
+		static void convertFromChar(char c);
+		static void convertFromInt(int i);
+		static void convertFromFloat(float f);
+		static void convertFromDouble(double d);
 
 	private:
 		ScalarConverte();
